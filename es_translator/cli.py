d68a35261c846f062e9dfbc8304a25299949ffd1
Pierre Romera <hello@pirhoo.com>
2019-05-17 15:40:29 +0200

    Added exit on error

--- es_translator/cli.py
+++ es_translator/cli.py
@@ -26,19 +26,25 @@ def print_done(str, quiet = False):
         logger.fatal(error, exc_info=True)
         print('{0} \033[91merror\033[0m'.format(str))
         if not quiet: raise error
+        sys.exit(1)
 
 def translate_hit(hit, apertium, options, index):
-    logger.info('Translating doc %s (%s)' % (index, hit.meta.id))
-    # Extract the value from a dict to avoid failing when the field is missing
-    translated_hit = TranslatedHit(hit, options['source_field'], options['target_field'])
-    translated_hit.add_translation(apertium)
-    logger.info('Translated doc %s (%s)' % (index, hit.meta.id))
-    # Skip on dry run
-    if not options['dry_run']:
-        # Create a new  client to
-        client = Elasticsearch(options['url'])
-        translated_hit.save(client)
-        logger.info('Saved translation for doc %s (%s)' % (index, hit.meta.id))
+    try:
+        logger.info('Translating doc %s (%s)' % (index, hit.meta.id))
+        # Extract the value from a dict to avoid failing when the field is missing
+        translated_hit = TranslatedHit(hit, options['source_field'], options['target_field'])
+        translated_hit.add_translation(apertium)
+        logger.info('Translated doc %s (%s)' % (index, hit.meta.id))
+        # Skip on dry run
+        if not options['dry_run']:
+            logger.info('Saving translation for doc %s (%s)' % (index, hit.meta.id))
+            # Create a new  client to
+            client = Elasticsearch(options['url'])
+            translated_hit.save(client)
+            logger.info('Saved translation for doc %s (%s)' % (index, hit.meta.id))
+    except Exception as e:
+        logger.warning('Unable to translate doc %s (%s)' % (index, hit.meta.id))
+
 
 def grouper(iterable, n):
     iterable = iter(iterable)
@@ -84,13 +90,11 @@ def main(**options):
     search = search.params(scroll=options['scan_scroll'])
 
     with print_done('Translating %s document(s)' % total_hits):
-        # We create a pool
-        with Pool(options['pool_size']) as p:
-            # Use scrolling mecanism from Elasticsearch to iterate over each result
-            # and we group search result in bucket of the size of the pool
-            for group_index, hit_group in enumerate(grouper(search.scan(), options['pool_size'])):
-                group_offset = group_index * options['pool_size']
-                hit_group = [[hit, apertium, options, group_offset + hit_index] for hit_index, hit in enumerate(hit_group)]
-                p.starmap(translate_hit, hit_group)
+        index = 0
+        # Use scrolling mecanism from Elasticsearch to iterate over each result
+        # and we group search result in bucket of the size of the pool
+        for hit in search.scan():
+            index = index + 1
+            translate_hit(hit, apertium, options, index)
 
     logger.info('Done!')
