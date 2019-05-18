14bff4bee9439ad61859de6d34798fda50734e48
Pierre Romera <hello@pirhoo.com>
2019-05-18 17:38:44 +0200

    Moved logic to a dedicated class

---
+++ es_translator/__init__.py
@@ -0,0 +1,96 @@
+
+import sys
+import itertools
+from contextlib import contextmanager
+from elasticsearch import Elasticsearch
+from elasticsearch_dsl import Search
+from es_translator.apertium import Apertium
+from es_translator.es import TranslatedHit
+from es_translator.logger import logger
+from multiprocessing import Pool
+
+def translation_worker(es_translator, hit, index):
+    try:
+        logger.info('Translating doc %s (%s)' % (index, hit.meta.id))
+        translated_hit = TranslatedHit(hit, es_translator.source_field, es_translator.target_field)
+        translated_hit.add_translation(es_translator.apertium)
+        logger.info('Translated doc %s (%s)' % (index, hit.meta.id))
+        # Skip on dry run
+        if not es_translator.dry_run:
+            # Create a new  client to
+            client = Elasticsearch(es_translator.url)
+            translated_hit.save(client)
+            logger.info('Saved translation for doc %s (%s)' % (index, hit.meta.id))
+    except Exception as e:
+        logger.warning('Unable to translate doc %s (%s)' % (index, hit.meta.id))
+
+class EsTranslator:
+    def __init__(self, options):
+        self.url = options['url']
+        self.index = options['index']
+        self.source_language = options['source_language']
+        self.target_language = options['target_language']
+        self.intermediary_language = options['intermediary_language']
+        self.source_field = options['source_field']
+        self.target_field = options['target_field']
+        self.query_string = options['query_string']
+        self.data_dir = options['data_dir']
+        self.scan_scroll = options['scan_scroll']
+        self.dry_run = options['dry_run']
+        self.pool_size = options['pool_size']
+
+    def start(self):
+        with self.print_done('Instantiating Apertium'):
+            self.apertium = self.init_apertium()
+            
+        with self.print_done('Translating %s document(s)' % self.search().execute().hits.total):
+            # Add missing field and change the scroll duration
+            search = self.search()
+            search = search.source([self.source_field, self.target_field, '_routing'])
+            search = search.params(scroll=self.scan_scroll)
+            # We create a pool
+            with Pool(self.pool_size) as pool:
+                # Use scrolling mecanism from Elasticsearch to iterate over each result
+                # and we group search result in bucket of the size of the pool
+                for group_index, hit_group in enumerate(self.grouper(search.scan(), self.pool_size)):
+                    group_offset = group_index * self.pool_size
+                    hit_group = [[self, hit, group_offset + hit_index] for hit_index, hit in enumerate(hit_group)]
+                    pool.starmap(translation_worker, hit_group)
+        logger.info('Done!')
+
+    def search(self):
+        es_client = Elasticsearch(self.url)
+        search = Search(index=self.index, using=es_client)
+        # Add query_string to the search
+        if self.query_string: search = search.query("query_string", query=self.query_string)
+        return search
+
+    def init_apertium(self):
+        return Apertium(self.source_language, self.target_language, self.intermediary_language, self.data_dir)
+
+    def print_flush(self, str):
+        sys.stdout.write('\r{0}'.format(str))
+        sys.stdout.flush()
+
+    def grouper(self, iterable, n):
+        iterable = iter(iterable)
+        while True:
+            tup = tuple(itertools.islice(iterable, 0, n))
+            if tup:
+                yield tup
+            else:
+                break
+
+    @contextmanager
+    def print_done(self, str, quiet = False):
+        logger.info(str)
+        str = '\r%s...' % str
+        self.print_flush(str)
+        try:
+            yield
+            print('{0} \033[92mdone\033[0m'.format(str))
+        except Exception as error:
+            logger.error(error, exc_info=True)
+            print('{0} \033[91merror\033[0m'.format(str))
+            if not quiet: raise error
+            sys.exit(1)
--- es_translator/cli.py
+++ es_translator/cli.py
@@ -1,68 +1,17 @@
 import click
-import sys
 
-import itertools
-from contextlib import contextmanager
-from elasticsearch import Elasticsearch
-from elasticsearch_dsl import Search
-from es_translator.apertium import Apertium
-from es_translator.es import TranslatedHit
-from es_translator.logger import logger, add_sysload_handler
-from multiprocessing import Pool
-
-def print_flush(str):
-    sys.stdout.write('\r{0}'.format(str))
-    sys.stdout.flush()
-
-@contextmanager
-def print_done(str, quiet = False):
-    logger.info(str)
-    str = '\r%s...' % str
-    print_flush(str)
-    try:
-        yield
-        print('{0} \033[92mdone\033[0m'.format(str))
-    except Exception as error:
-        logger.fatal(error, exc_info=True)
-        print('{0} \033[91merror\033[0m'.format(str))
-        if not quiet: raise error
-        sys.exit(1)
-
-def translate_hit(hit, apertium, options, index):
-    logger.info('Translating doc %s (%s)' % (index, hit.meta.id))
-    try:
-        # Extract the value from a dict to avoid failing when the field is missing
-        translated_hit = TranslatedHit(hit, options['source_field'], options['target_field'])
-        translated_hit.add_translation(apertium)
-        logger.info('Translated doc %s (%s)' % (index, hit.meta.id))
-        # Skip on dry run
-        if not options['dry_run']:
-            # Create a new  client to
-            client = Elasticsearch(options['url'])
-            translated_hit.save(client)
-            logger.info('Saved translation for doc %s (%s)' % (index, hit.meta.id))
-    except Exception as e:
-        logger.warning('Unable to translate doc %s (%s)' % (index, hit.meta.id))
-
-
-def grouper(iterable, n):
-    iterable = iter(iterable)
-    while True:
-        tup = tuple(itertools.islice(iterable, 0, n))
-        if tup:
-            yield tup
-        else:
-            break
+from es_translator import EsTranslator
+from es_translator.logger import add_sysload_handler
 
 @click.command()
-@click.option('--url', required=True, help='Elastichsearch URL')
-@click.option('--index', required=True, help='Elastichsearch Index')
-@click.option('--source-language', required=True, help='Source language to translate from')
-@click.option('--target-language', required=True, help='Target language to translate to')
+@click.option('--url', help='Elastichsearch URL', required=True)
+@click.option('--index', help='Elastichsearch Index', required=True)
+@click.option('--source-language', help='Source language to translate from', required=True)
+@click.option('--target-language', help='Target language to translate to', required=True)
 @click.option('--intermediary-language', help='An intermediary language to use when no translation is available between the source and the target. If none is provided this will be calculated automaticly.')
 @click.option('--source-field', help='Document field to translate', default="content")
 @click.option('--target-field', help='Document field to translate', default="content_translated")
-@click.option('--query', help='Search query string to filter result')
+@click.option('--query-string', help='Search query string to filter result')
 @click.option('--data-dir', help='Path to the directory where to language model will be downloaded')
 @click.option('--scan-scroll', help='Scroll duration (set to higher value if you\'re processing a lot of documents)', default="5m")
 @click.option('--dry-run', help='Don\'t save anything in Elasticsearch', is_flag=True, default=False)
@@ -70,32 +19,6 @@ def grouper(iterable, n):
 @click.option('--syslog-address', help='Syslog address', default='localhost')
 @click.option('--syslog-port', help='Syslog port', default=514)
 @click.option('--syslog-facility', help='Syslog facility', default='local7')
-def main(**options):
-    # Configure syslog
-    add_sysload_handler(options['syslog_address'], options['syslog_port'], options['syslog_facility'])
-
-    with print_done('Instantiating Apertium'):
-        apertium = Apertium(options['source_language'], options['target_language'], options['intermediary_language'], options['data_dir'])
-    # Build the search
-    client = Elasticsearch(options['url'])
-    search = Search(index=options['index'], using=client)
-    # Add query_string to the search
-    if options['query']:
-        search = search.query("query_string", query=options['query'])
-
-    total_hits = search.execute().hits.total
-    # Add missing field and change the scroll duration
-    search = search.source([options['source_field'], options['target_field'], '_routing'])
-    search = search.params(scroll=options['scan_scroll'])
-
-    with print_done('Translating %s document(s)' % total_hits):
-        # We create a pool
-        with Pool(options['pool_size']) as pool:
-            # Use scrolling mecanism from Elasticsearch to iterate over each result
-            # and we group search result in bucket of the size of the pool
-            for group_index, hit_group in enumerate(grouper(search.scan(), options['pool_size'])):
-                group_offset = group_index * options['pool_size']
-                hit_group = [[hit, apertium, options, group_offset + hit_index] for hit_index, hit in enumerate(hit_group)]
-                pool.starmap(translate_hit, hit_group)
-
-    logger.info('Done!')
+def main(syslog_address, syslog_port, syslog_facility, **options):
+    add_sysload_handler(syslog_address, syslog_port, syslog_facility)
+    EsTranslator(options).start()
