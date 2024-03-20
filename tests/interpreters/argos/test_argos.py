841c06f3845b91b8de281c6e14db55d71d0cf73b
Pierre Romera <hello@pirhoo.com>
2024-03-20 22:13:00 +0000

    refactor: lint

--- tests/interpreters/argos/test_argos.py
+++ tests/interpreters/argos/test_argos.py
@@ -12,24 +12,22 @@ class TestArgos(TestCase):
         self.package.from_code = self.spa2eng.source_alpha_2
         self.package.to_code = self.spa2eng.target_alpha_2
 
-  
     def test_has_eng_to_spa_is_supported(self):
         self.assertTrue(self.eng2spa.is_pair_available)
-    
+
     def test_has_spa_to_eng_is_supported(self):
         self.assertTrue(self.spa2eng.is_pair_available)
-        
+
     def test_has_fra_to_eng_is_supported(self):
         self.assertTrue(self.fra2eng.is_pair_available)
 
     def test_has_ava_to_kau_isnt_supported(self): 
         with self.assertRaises(Exception):
-          self.argos = Argos(source = 'ava', target = 'kau')
-          self.assertFalse(self.argos.is_pair_available)
+            self.argos = Argos(source = 'ava', target = 'kau')
 
     def test_translation_from_fra_to_eng(self):
-        self.assertEqual(self.fra2eng.translate('bonjour!').lower(), 'hello!')
-            
+        self.assertEqual(self.fra2eng.translate("bonjour!").lower(), "hello!")
+
     def test_translation_from_de_to_en(self):
         self.assertEqual(self.fra2eng.translate('bonjour monsieur, comment ça va ?'), 'Hello, sir, how are you?')
 
